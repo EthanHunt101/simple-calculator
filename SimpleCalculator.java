6f71407d1cbe16e0b3830fec86965685c489e80e
Sphinx530 <ethandouglashunt@gmail.com>
2025-09-11 00:10:48 -0400

    progress

--- SimpleCalculator.java
+++ SimpleCalculator.java
@@ -1,15 +1,17 @@
 import java.util.Scanner;
 
 public class SimpleCalculator {
-    private int numberOne;
-    private int numberTwo;
+    private double numberOne;
+    private double numberTwo;
     private char operator;
+    private double result;
+    private boolean validOperator;
 
-    public int getNumberOne() {
+    public double getNumberOne() {
         return this.numberOne;
     }
 
-    public int getNumberTwo() {
+    public double getNumberTwo() {
         return this.numberTwo;
     }
 
@@ -17,11 +19,15 @@ public class SimpleCalculator {
         return this.operator;
     }
 
-    public void setNumberOne(int numberOne) {
+    public double getResult() {
+        return this.result;
+    }
+
+    public void setNumberOne(double numberOne) {
         this.numberOne = numberOne;
     }
 
-    public void setNumberTwo(int numberTwo) {
+    public void setNumberTwo(double numberTwo) {
         this.numberTwo = numberTwo;
     }
 
@@ -29,12 +35,35 @@ public class SimpleCalculator {
         this.operator = operator;
     }
 
+    public void setResult(double result) {
+        this.result = result;
+    }
+
+
+
     public static void main(String[] args) {     
         Scanner scanner = new Scanner(System.in);
-        System.out.println("e");
 
+        SimpleCalculator calc = new SimpleCalculator();
 
+        System.out.print("Enter the first number: ");
+        calc.setNumberOne(scanner.nextDouble());
 
+        System.out.print("Enter an operator (+, -, *,/, ^)");
+        calc.setOperator(scanner.next().charAt(0));
+
+        System.out.print("Enter the second number: ");
+        calc.setNumberTwo(scanner.nextDouble());
+
+        switch(calc.getOperator()) {
+            case '+' -> calc.setResult(calc.getNumberOne() + calc.getNumberTwo());
+            default -> {
+                System.out.println("This is an invalid operator");
+            }
+        }
+        if(validOperator) {
+            System.out.println(calc.getResult());
+        }
 
         scanner.close();
     }
