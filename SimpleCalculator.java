bc18dd3dc00c47ebdf2c1c5f7e64f43f2a942c14
Sphinx530 <ethandouglashunt@gmail.com>
2025-09-10 22:49:40 -0400

    created getters and setters

--- SimpleCalculator.java
+++ SimpleCalculator.java
@@ -1,3 +1,29 @@
 public class SimpleCalculator {
+    private int numberOne;
+    private int numberTwo;
+    private char operator;
 
+    public int getNumberOne() {
+        return this.numberOne;
+    }
+
+    public int getNumberTwo() {
+        return this.numberTwo;
+    }
+
+    public char getOperator() {
+        return this.operator;
+    }
+
+    public void setNumberOne(int numberOne) {
+        this.numberOne = numberOne;
+    }
+
+    public void setNumberTwo(int numberTwo) {
+        this.numberTwo = numberTwo;
+    }
+
+    public void setOperator(char operator) {
+        this.operator = operator;;
+    }
 }
